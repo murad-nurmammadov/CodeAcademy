5b9958e9d81fbd1f05e5ddc22a6700a87c96c45d
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-03-12 02:46:59 +0400

    (12.03.2025) Task 4

--- Program.cs
+++ Program.cs
@@ -38,15 +38,51 @@
 // 2 ədəd daxil olunur, əgər ədədlərin hər ikisi 2'yə qalıqsız bölünürsə, cəmini
 // tapırsınız, əks halda ekrana "daxil olunan ədədlər cüt olmalıdır!" yazısını yazdırırsınız.
 
-int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = Convert.ToInt32(Console.ReadLine());
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 % 2 == 0 && num2 % 2 == 0)
+//if (num1 % 2 == 0 && num2 % 2 == 0)
+//{
+//    Console.WriteLine(num1 + num2);
+//}
+//else
+//{
+//    Console.WriteLine("daxil olunan ededler cut olmalidir!");
+//}
+#endregion
+
+#region Task 4
+// Tələbə balını daxil edir, bu bala uyğun, hərf balı tapırsınız.
+// (Məsələn: 91 -> A, 75 -> C)
+
+int score = Convert.ToInt32(Console.ReadLine());
+
+if (score > 100 || score < 0)
 {
-    Console.WriteLine(num1 + num2);
+    Console.WriteLine("Invalid score");
+}
+else if (score >= 91)
+{
+    Console.WriteLine("A");
+}
+else if (score >= 81)
+{
+    Console.WriteLine("B");
+}
+else if (score >= 71)
+{
+    Console.WriteLine("C");
+}
+else if (score >= 61)
+{
+    Console.WriteLine("D");
+}
+else if (score >= 51)
+{
+    Console.WriteLine("E");
 }
 else
 {
-    Console.WriteLine("daxil olunan ededler cut olmalidir!");
+    Console.WriteLine("F");
 }
 #endregion
