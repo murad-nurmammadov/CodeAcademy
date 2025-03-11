a691ba1017d58058eefc36ab231b4cff5fefee81
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-03-12 02:28:04 +0400

    (12.03.2025) Task 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 1
+// Username və password daxil olunur, username dəyəri "Admin" və password dəyəri "Admin123"-dürsə
+// ekrana"Xoş gəlmisiniz!", əks halda "UserName və ya password yanlışdır!" yazısını yazdırın.
+
+String username = Console.ReadLine();
+String password = Console.ReadLine();
+
+if (username == "Admin" && password == "Admin123")
+{
+    Console.WriteLine("Xoş gəlmisiniz!");
+}
+else
+{
+    Console.WriteLine("UserName və ya password yanlışdır!");
+}
