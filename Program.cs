4277379ff1a8b8224be683251795f59d3cb2e68c
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-03-12 02:42:20 +0400

    (12.03.2025) Task 3

--- Program.cs
+++ Program.cs
@@ -18,21 +18,35 @@
 #region Task 2
 // Bir dəyişən təyin olunur, ədədin cüt və ya tək olduğu ekrana çap olunur
 
-int num = Convert.ToInt32(Console.ReadLine());
+//int num = Convert.ToInt32(Console.ReadLine());
 
-if (num == 0)
+//if (num == 0)
+//{
+//    Console.WriteLine("Neither odd, nor even");
+//}
+//else if (num % 2 == 0)
+//{
+//    Console.WriteLine("Even");
+//}
+//else
+//{
+//    Console.WriteLine("Odd");
+//}
+#endregion
+
+#region Task 3
+// 2 ədəd daxil olunur, əgər ədədlərin hər ikisi 2'yə qalıqsız bölünürsə, cəmini
+// tapırsınız, əks halda ekrana "daxil olunan ədədlər cüt olmalıdır!" yazısını yazdırırsınız.
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0 && num2 % 2 == 0)
 {
-    Console.WriteLine("Neither odd, nor even");
-}
-else if (num % 2 == 0)
-{
-    Console.WriteLine("Even");
+    Console.WriteLine(num1 + num2);
 }
 else
 {
-    Console.WriteLine("Odd");
+    Console.WriteLine("daxil olunan ededler cut olmalidir!");
 }
 #endregion
-
-
-
