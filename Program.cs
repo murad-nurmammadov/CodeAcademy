25a77a94959f217c75a502cabf0641a68d2c1814
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-03-13 02:52:53 +0400

    (13.03.2025) Task 2

--- Program.cs
+++ Program.cs
@@ -2,18 +2,43 @@
 // Verilmish ededin mertebelerinin en boyuyunu tapan alqoritm
 // Misal: input 7481, output 8
 
-int num = Convert.ToInt32(Console.ReadLine());
-int maxDigit = 0;
+//int num = Convert.ToInt32(Console.ReadLine());
+//int maxDigit = 0;
 
-while (num > 0)
+//while (num > 0)
+//{
+//    int digit = num % 10;
+//    if (digit > maxDigit)
+//    {
+//        maxDigit = digit;
+//    }
+//    num /= 10;
+//}
+
+//Console.WriteLine(maxDigit);
+#endregion
+
+#region Task2
+// Verilmish ededin 2 quvveti olub olmadigini tapan alqoritm
+// Misal: input 8 output 2-in quvvetidir ve ya input 9 2-in quvveti deyil
+
+int num = Convert.ToInt32(Console.ReadLine());
+int powerOfTwo = 1;
+bool isPowerOfTwo = false;
+
+while (powerOfTwo <= num)
 {
-    int digit = num % 10;
-    if (digit > maxDigit)
+    if (powerOfTwo == num)
     {
-        maxDigit = digit;
+        isPowerOfTwo = true;
+        Console.WriteLine(num + " is a power of two");
+        break;
     }
-    num /= 10;
+    powerOfTwo *= 2;
 }
 
-Console.WriteLine(maxDigit);
+if (!isPowerOfTwo)
+{
+    Console.WriteLine(num + " is NOT a power of two");
+}
 #endregion
