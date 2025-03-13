e024dd890da5f124c1dbee8132ad7943c8fbab6c
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-03-13 23:00:07 +0400

    (14.03.2025) Task 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello");
+﻿namespace CodeAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            string text = Console.ReadLine();
+            string reversedText = ReverseText(text);
+            Console.WriteLine(reversedText);
+        }
+
+        #region Task 1
+        // Daxil etdiyimiz mətni tərsinə çevirən metod yazın;
+        // meselen: Hello World! => !dlroW olleH
+
+        static string ReverseText(string text)
+        {
+            string reversedText = "";
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reversedText += text[i];
+            }
+
+            return reversedText;
+        }
+        #endregion
+    }
+}
