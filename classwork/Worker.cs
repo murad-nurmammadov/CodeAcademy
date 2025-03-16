430fe8e657b41d442bd05873d004eb8baed5f180
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-03-16 09:12:08 +0400

    Classwork: (14.03.2025) Task 1

---
+++ classwork/Worker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAcademy.classwork
+{
+    internal class Worker
+    {
+        // Properties
+        public string Name;
+        public string Surname;
+        public byte Experience;
+        public float Payment;
+
+        // Constructors
+        public Worker(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public Worker(string name, string surname, byte experience) : this(name, surname)
+        {
+            Experience = experience;
+        }
+
+        public Worker(string name, string surname, byte age, byte experience, float payment) : this(name, surname, experience)
+        {
+            Payment = payment;
+        }
+    }
+}
