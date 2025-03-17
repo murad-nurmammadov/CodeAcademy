5edbdc3775f07f5c3a5d0aa0edd56afbf604d556
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-03-17 17:36:07 +0400

    (17.03.2025) Methods-2: Task 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main()
         {
+            // Methods (2)
 
+            // Task 1
+            // Method Task: Min methodu yaradılır.Daxil olunan arrayın ən kiçik elementini tapır.
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, };
+            Console.WriteLine(Min(nums));
+        }
+
+        static int Min(int[] nums)
+        {
+            int min = nums[0];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < min) { min = nums[i]; }
+            }
+
+            return min;
         }
     }
 }
