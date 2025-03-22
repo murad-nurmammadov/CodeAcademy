eacd52f6ce015e1e99bcb5725cfad68a5be77c6e
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-03-22 18:41:07 +0400

    Moved Facebook project out of homeworks folder

---
+++ Facebook/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeAcademy.Facebook
+{
+    class Comment
+    {
+        // Fields
+        public string Text;
+        public DateTime Date;
+        public User[] LikingUsers;
+
+        // Constructor
+        public Comment(string text)
+        {
+            Text = text;
+            Date = DateTime.Now;
+        }
+    }
+}
