c230ee96a89bca1b6247486d9744a3dea9164d87
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-04-01 23:39:16 +0400

    Homework: Helper class (Extension)

---
+++ Homeworks/ExtensionsHW/Extensions/Helper.cs
@@ -0,0 +1,57 @@
+﻿namespace Extensions
+{
+    public static class Helper
+    {
+        /*
+        IsOdd() -- int deyerler üçün həmin obyektin tək ədəd olub olmadığını geri qaytaran (true/false) method
+        IsEven() -- int  deyerler  üçün həmin obyektin cüt ədəd olub olmadığını geri qaytaran (true/false) method
+        HasDigit() -- string  deyerler  üçün həmin obyektin daxilində digit character olub olmamasını geri qaytaran(true/false) metod
+        CheckPassword() -- string  deyerlerini yoxluyub true/false qaytarir Password-da en azi bir boyuk bir kicik herf ve en azi 1 reqem olamilidir. Uzunlugu 8-den kicik olmamalidir. 
+        Capitalize() -- String-lər üçün Capitalize adlı extention metod yaradırsınız.Bu metod string dəyərini capitalaize edib geri
+        */
+        public static bool IsOdd(this int num)
+        {
+            if (num % 2 == 1) { return true; }
+            else { return false; }
+        }
+
+        public static bool IsEven(this int num)
+        {
+            if (num % 2 == 0) { return true; }
+            else { return false; }
+        }
+
+        public static bool HasDigit(this string text)
+        {
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c)) { return true; }
+            }
+            return false;
+        }
+
+        public static bool CheckPassword(this string password)
+        {
+            bool hasDigit = password.HasDigit();
+            bool hasUpper = false;
+            bool hasLower = false;
+
+            foreach (char c in password)
+            {
+                if (char.IsLower(c)) { hasLower = true; }
+                else if (char.IsUpper(c)) { hasUpper = true; }
+            }
+
+            bool hasValidLength = password.Length > 8;
+            bool hasRequireChars = hasDigit && hasUpper && hasLower;
+
+            if (hasValidLength && hasRequireChars) { return true; }
+            else { return false; }
+        }
+
+        public static string Capitalize(this string text)
+        {
+            return char.ToUpper(text[0]) + text.ToLower()[1..];
+        }
+    }
+}
---
+++ Homeworks/ExtensionsHW/ExtensionsHW/Program.cs
@@ -0,0 +1,24 @@
+﻿using Extensions;
+
+namespace ExtensionsHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 21;
+            Console.WriteLine(age.IsOdd());
+            Console.WriteLine(age.IsEven());
+
+            string password1 = "Murad2003";
+            string password2 = "nmurad";
+            Console.WriteLine(password1.HasDigit());
+            Console.WriteLine(password2.HasDigit());
+            Console.WriteLine(password1.CheckPassword());
+            Console.WriteLine(password2.CheckPassword());
+
+            string name = "mUrAd";
+            Console.WriteLine(name.Capitalize());
+        }
+    }
+}
