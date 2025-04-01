776b1db337dc6b1b6aecaedb255f33cdb107dcce
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-04-01 22:30:25 +0400

    Homework: Access Modifiers - 3

---
+++ Homeworks/AccessModifers3/AccessModifers3/Program.cs
@@ -0,0 +1,43 @@
+﻿using Models;
+
+namespace AccessModifers3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Aşağıdakı memberlərdən ibarət User class yaradın
+             - UserName - istifadəçinin istifadəçi adını ifadə edir
+             - Password - İstifadəçinin şifrə dəyərini ifadə edir
+
+            UserName dəyəri təyin olunmamış User obyekti yaradıla bilinməməlidir.
+            UserName dəyərinin uzunluğu minimum 6, maksimum 25 ola bilər.
+
+            Password dəyərinin uzunluğu minimum 8 , maksimum 25 ola bilər və 
+            içərisində mütlər ən az 1 böyük, 1 kiçik hərf və 1 rəqəm olmalıdır.
+
+            Daha sonra User-dan instance yaradın, bunun üçün username və password 
+            dəyərlərini console-dan qəbul edin.
+
+            User classin icinde asagidaki metodlar da olsun:
+              - HasDigit - parametr olaraq string dəyər qəbul edib o dəyərdə rəqəm 
+                varsa geriyə true yoxdusa false qaytaran metod
+              - HasUpper -  parametr olaraq string dəyər qəbul edib o dəyərdə 
+                uppercase varsa geriyə true yoxdursa false qaytaran metod
+              - HasLower - parametr olaraq string dəyər qəbul edib o dəyərdə 
+                lowercase varsa geriyə true yoxdursa false qaytaran metod
+            */
+
+            Console.WriteLine("Enter username:");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine();
+
+            User user = new User(username, password);
+
+            Console.WriteLine($"{user.Username} - {user.Password}");
+        }
+    }
+}
---
+++ Homeworks/AccessModifers3/Models/User.cs
@@ -0,0 +1,75 @@
+﻿namespace Models
+{
+    public class User
+    {
+        // Fields
+        private string _username;
+        private string _password;
+
+        // Properties
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25)
+                {
+                    _username = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                bool hasValidLength = value.Length >= 8 && value.Length <= 25;
+                bool hasRequiredChars = HasDigit(value) && HasLower(value) && HasUpper(value);
+
+                if (hasValidLength && hasRequiredChars)
+                {
+                    _password = value;
+                }
+            }
+        }
+
+        // Constructor
+        public User(string usernname, string password)
+        {
+            Username = usernname;
+            Password = password;
+        }
+
+        // Methods
+        private bool HasDigit(string text)
+        {
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c)) { return true; }
+            }
+
+            return false;
+        }
+
+        private bool HasUpper(string text)
+        {
+            foreach (char c in text)
+            {
+                if (char.IsUpper(c)) { return true; }
+            }
+
+            return false;
+        }
+
+        private bool HasLower(string text)
+        {
+            foreach (char c in text)
+            {
+                if (char.IsLower(c)) { return true; }
+            }
+
+            return false;
+        }
+    }
+}
