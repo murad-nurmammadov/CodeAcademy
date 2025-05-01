fe7a1e1c80b46325461f565c104661344453789f
Murad Nurmammadov <muradin-bb104@code.edu.az>
2025-05-01 13:45:00 +0400

    CW Pronia: db-first approach

---
+++ Classworks/FrontToBack/ProniaTemplate/ProniaTemplate/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ProniaTemplate.Models;
+
+namespace ProniaTemplate.ViewModels;
+
+public class HomeViewModel
+{
+    public List<Slider> Sliders{ get; set; }
+    public List<Service> Services{ get; set; }
+}
