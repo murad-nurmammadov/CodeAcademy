314cbc1e9ef1230b8cc43c2c6ced0405f53221e4
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-05-05 18:34:37 +0400

    Techan: brands controller, CRUD

--- Shared/Techan/Techan/Techan/Areas/Admin/Controllers/BrandController.cs
+++ Shared/Techan/Techan/Techan/Areas/Admin/Controllers/BrandController.cs
@@ -1,19 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Techan.Contexts;
 using Techan.Models;
 
 namespace Techan.Areas.Admin.Controllers;
 
 [Area("Admin")]
-public class BrandController : Controller
+public class BrandController(TechanDbContext _context) : Controller
 {
+    public async Task<IActionResult> Index()
+    {
+        List<Brand> brands = await _context.Brands.ToListAsync();
+        return View(brands);
+    }
+
     public IActionResult Create()
     {
         return View();
     }
 
-    [HttpPost]
-    public IActionResult Create(Slider slider)
+    [HttpPost, AutoValidateAntiforgeryToken]
+    public async Task<IActionResult> Create(Brand brand)
     {
-        return View();
+        if (!ModelState.IsValid)
+            return View(brand);
+        
+        await _context.Brands.AddAsync(brand);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Create));
+    }
+
+    public async Task<IActionResult> Update(int id)
+    {
+        Brand? brand = await _context.Brands.FindAsync(id);
+
+        if (brand == null)
+            return NotFound();
+
+        return View(brand);
+    }
+
+    [HttpPost, AutoValidateAntiforgeryToken]
+    public async Task<IActionResult> Update(Brand brand)
+    {
+        if (!ModelState.IsValid)
+            return View(brand);
+
+        _context.Brands.Update(brand);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost, AutoValidateAntiforgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        Brand? brand = await _context.Brands.FindAsync(id);
+
+        if (brand == null)
+            return NotFound();
+
+        _context.Remove(brand); 
+        await _context.SaveChangesAsync();
+        
+        return RedirectToAction(nameof(Index));
     }
 }
---
+++ Shared/Techan/Techan/Techan/ViewModels/BrandVMs/BrandCreateVM.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Techan.ViewModels.BrandVM;
+
+public class BrandCreateVM
+{
+    [MaxLength(100)]
+    public string Name { get; set; }
+}
