a5ffd9c30dbad362d983c87fb4c3c28274a25db0
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2025-05-27 15:41:09 +0400

    Techan: added file extension class

---
+++ Shared/Techan/Techan/Extensions/FileExtension.cs
@@ -0,0 +1,32 @@
+﻿namespace Techan.Extensions;
+
+public static class FileExtension
+{
+    public static bool hasValidType(this IFormFile file, string type)
+    {
+        return file.ContentType.StartsWith(type);
+    }
+
+    public static bool hasValidSize(this IFormFile file, int kb = 2048)  // 2 MB by default
+    {
+        return file.Length <= kb * 1024;
+    }
+
+    public static async Task<string> UploadAsync(this IFormFile file, string rootPath)
+    {
+        string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+        string fullPath = Path.Combine(rootPath, fileName);
+
+        using var fs = new FileStream(fullPath, FileMode.Create);
+        await file.CopyToAsync(fs);
+
+        return fullPath;
+    }
+
+    public static async Task UploadAsync(this IFormFile file, string rootPath, string fileName)
+    {
+        string fullPath = Path.Combine(rootPath, fileName);
+        using var fs = new FileStream(fullPath, FileMode.Create);
+        await file.CopyToAsync(fs);
+    }
+}
--- Shared/Techan/Techan/Program.cs
+++ Shared/Techan/Techan/Program.cs
@@ -3,54 +3,62 @@ using Microsoft.EntityFrameworkCore;
 using Techan.Contexts;
 using Techan.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Techan;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<TechanDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("default")));
-
-builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+public class Program
 {
-    options.User.RequireUniqueEmail = true;
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
 
-    options.Password.RequiredLength = 7;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireDigit = false;
-})
-.AddEntityFrameworkStores<TechanDbContext>()
-.AddDefaultTokenProviders();
+        builder.Services.AddDbContext<TechanDbContext>(opt =>
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("default")));
 
-var app = builder.Build();
+        builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+
+            options.Password.RequiredLength = 7;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireDigit = false;
+        })
+        .AddEntityFrameworkStores<TechanDbContext>()
+        .AddDefaultTokenProviders();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+        app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "areas",
-    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
